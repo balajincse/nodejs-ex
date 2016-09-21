89e729b76e2ead7ef271d8e92ffed7b11024d4a5
Balaji <balajincse@outlook.com>
2016-09-21 18:43:39 +0530

    update server.js

--- server.js
+++ server.js
@@ -95,80 +95,80 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
+//  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+//      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+//      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+//      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+//      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+//      mongoUser = process.env[mongoServiceName + '_USER'];
 
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
+//  if (mongoHost && mongoPort && mongoDatabase) {
+//    mongoURLLabel = mongoURL = 'mongodb://';
+//    if (mongoUser && mongoPassword) {
+//      mongoURL += mongoUser + ':' + mongoPassword + '@';
+//    }
     // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+//    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+//    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
 
-  }
-}
+//  }
+//}
 //var db = null,
 //    dbDetails = new Object();
 
 //var initDb = function(callback) {
-  if (mongoURL == null) return;
+//  if (mongoURL == null) return;
 
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
+//  var mongodb = require('mongodb');
+//  if (mongodb == null) return;
 
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
+//  mongodb.connect(mongoURL, function(err, conn) {
+//   if (err) {
+//      callback(err);
+//      return;
+//    }
 
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
+//    db = conn;
+//    dbDetails.databaseName = db.databaseName;
+//    dbDetails.url = mongoURLLabel;
+//    dbDetails.type = 'MongoDB';
 
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
+//    console.log('Connected to MongoDB at: %s', mongoURL);
+//  });
+//};
 
 //app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
+//  if (!db) {
+//    initDb(function(err){});
+//  }
+//  if (db) {
+//    var col = db.collection('counts');
     // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
+//    col.insert({ip: req.ip, date: Date.now()});
+//    col.count(function(err, count){
+//      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+//    });
+//  } else {
+//    res.render('index.html', { pageCountMessage : null});
+//  }
+//});
 
 //app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
+//  if (!db) {
+//    initDb(function(err){});
+//  }
+//  if (db) {
+//    db.collection('counts').count(function(err, count ){
+//      res.send('{ pageCount: ' + count + '}');
+//    });
+//  } else {
+//    res.send('{ pageCount: -1 }');
+//  }
+//});
 
 // error handling
 app.use(function(err, req, res, next){
@@ -177,8 +177,8 @@ app.use(function(err, req, res, next){
 });
 
 //initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
+//  console.log('Error connecting to Mongo. Message:\n'+err);
+//});
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
